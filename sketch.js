751c59e2fc64cd4a9638d0dbe051f93c6c4fcdcf
Quasimonastic <Quasimonastic@users.noreply.github.com>
2016-06-19 14:30:37 -0700

    Particles Gravity with Wind

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var particles = [1,2,3,4];
+
+function setup() {
+  createCanvas(750, 810);
+  for (var i = 0; i < 500; i++) {
+    particles[i] = new Particle(random(0.5,3), random(1,width), random(1,height));
+    };
+  }
+
+function draw() {
+  background(35);
+  text("test = " + particles[1],25,25);
+
+  var wind = createVector(random(0.1, 0.4),0);
+
+  for (var i = 0; i < particles.length; i++) {
+  // Gravity is scaled by mass here!
+  var gravity = createVector(0, random(-0.03, 0.03) * particles[i].mass);
+  // Apply gravity
+  particles[i].applyForce(gravity);
+  /*Adding a force you can make up anything you want to*/
+  if (mouseIsPressed){
+    particles[i].applyForce(wind);
+  }
+
+  // Update and display
+  particles[i].update();
+  particles[i].display();
+  particles[i].checkEdges();
+  }
+}
